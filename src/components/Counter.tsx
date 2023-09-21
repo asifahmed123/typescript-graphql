8aede097901ce7326a4af4c624f7068f3ff89d07
asifahmed123 <ahmedasif092@gmail.com>
2023-09-21 16:12:24 +0600

    increment count using useReducer

---
+++ src/components/Counter.tsx
@@ -0,0 +1,81 @@
+import { useReducer } from "react";
+
+interface initialStateType {
+  increment: number;
+  reset: number;
+  decrement: number;
+}
+
+interface ActionType {
+  type: "increment" | "reset" | "decrement";
+  payload: number;
+}
+
+const initialState: initialStateType = {
+  increment: 0,
+  reset: 0,
+  decrement: 0,
+};
+
+const reducer = (state: initialStateType, action: ActionType) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, increment: state.increment + action.payload }; // Fix here
+    case "reset":
+      return { ...state, increment: 0 }; // Fix here
+    case "decrement":
+      return { ...state, decrement: state.decrement - 1 }; // Fix here
+
+    default:
+      throw new Error("Unhandled action type: " + action.type);
+  }
+};
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="w-full h-[100vh] mx-auto flex flex-col justify-center items-center">
+      <h2 className="bg-lime-900 w-40 text-center p-2 rounded mb-10 font-bold">
+        Count: {state.increment} {/* Fix here */}
+      </h2>
+      <div>
+        <button
+          onClick={() =>
+            dispatch({
+              type: "increment",
+              payload: 1,
+            })
+          }
+          className="p-2 btn-accent rounded mx-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() =>
+            dispatch({
+              type: "reset",
+              payload: 0, // You can omit payload for reset
+            })
+          }
+          className="p-2 btn-warning rounded mx-2"
+        >
+          Reset
+        </button>
+        <button
+          onClick={() =>
+            dispatch({
+              type: "decrement",
+              payload: 1,
+            })
+          }
+          className="p-2 btn-info rounded mx-2"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
